acaab7d5f63044feb2264a7bcb2660bd311e8083
maxim-gaiduchek <maxim.agyduchek@gmail.com>
2022-12-26 22:41:03 +0100

    feature(lab5): Release saving cookies

--- lab5/script.js
+++ lab5/script.js
@@ -1,6 +1,25 @@
+let numbers = [];
+
+let numbersInput = document.getElementById("numbers-input");
+let numbersContainer = document.getElementById("numbers-container");
+let numbersForm = document.getElementById("numbers-form");
+
 window.addEventListener("load", () => {
     swapXY();
     calcSquare();
+
+    numbersForm.style.display = "none";
+
+    if (document.cookie !== "") {
+        let data = document.cookie.split(";")[0].split("=")[1].split(" ");
+
+        if (confirm(`Min: ${data[0]}, Max: ${data[1]}\n\nWant to save this data again? Then you will needed to reload page`)) {
+            return;
+        }
+    }
+
+    document.cookie = "minmax=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    numbersForm.style.display = "block";
 });
 
 function swapXY() {
@@ -10,7 +29,32 @@ function swapXY() {
 }
 
 function calcSquare() {
-    let x = 100, y = 20.331;
+    let height = 100, width = 20.331;
 
-    document.getElementById("square-result").innerText = x * y;
+    document.getElementById("square-result").innerText = height * width;
 }
+
+numbersForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (!numbersInput.value) {
+        return;
+    }
+
+    numbers.push(Number.parseInt(numbersInput.value));
+    document.getElementById("numbers-input").value = "";
+
+    numbersContainer.innerText = numbers.join(", ");
+
+    if (numbers.length < 10) {
+        numbersContainer.innerText += ` (${10 - numbers.length} left)`;
+    } else {
+        let min = Math.min(...numbers), max = Math.max(...numbers);
+
+        numbersContainer.innerText += ` (min and max are found!)`;
+        numbers = [];
+        document.cookie = `minmax=${min} ${max}`;
+
+        alert(`Min: ${min}, Max: ${max}`);
+    }
+});
