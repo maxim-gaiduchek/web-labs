e49bbd73b5ae9c584a479bbbe7a308549fc1ba5a
maxim-gaiduchek <maxim.agyduchek@gmail.com>
2023-01-06 18:30:39 +0100

    feature(lab5): Solve task 4

--- lab5/script.js
+++ lab5/script.js
@@ -4,10 +4,17 @@ let numbersInput = document.getElementById("numbers-input");
 let numbersContainer = document.getElementById("numbers-container");
 let numbersForm = document.getElementById("numbers-form");
 
+let boldTextContainer = document.getElementById("bold-text-container");
+let normalTextInput = document.getElementById("normal-text-input");
+let boldTextInput = document.getElementById("bold-text-input");
+let bold = false;
+
 window.addEventListener("load", () => {
+    // tasks 1, 2
     swapXY();
     calcSquare();
 
+    // task 3
     numbersForm.style.display = "none";
 
     if (document.cookie !== "") {
@@ -20,20 +27,36 @@ window.addEventListener("load", () => {
 
     document.cookie = "minmax=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
     numbersForm.style.display = "block";
+
+    // task 4
+    bold = localStorage.getItem("bold") === "bold";
+
+    if (bold) {
+        boldTextContainer.classList.remove("normal");
+        boldTextContainer.classList.add("bold");
+        boldTextInput.checked = true;
+    } else {
+        boldTextContainer.classList.remove("bold");
+        boldTextContainer.classList.add("normal");
+        normalTextInput.checked = true;
+    }
 });
 
+// task 1
 function swapXY() {
     let temp = document.getElementById("x").innerText;
     document.getElementById("x").innerText = document.getElementById("y").innerText;
     document.getElementById("y").innerText = temp;
 }
 
+// task 2
 function calcSquare() {
     let height = 100, width = 20.331;
 
     document.getElementById("square-result").innerText = `${height} * ${width} = ${height * width}`;
 }
 
+// task 3
 numbersForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -57,4 +80,25 @@ numbersForm.addEventListener("submit", (e) => {
 
         alert(`Min: ${min}, Max: ${max}`);
     }
+});
+
+// task 4
+boldTextContainer.addEventListener("focus", () => {
+    if (bold) {
+        boldTextContainer.classList.remove("normal");
+        boldTextContainer.classList.add("bold");
+        localStorage.setItem("bold", "bold");
+    } else {
+        boldTextContainer.classList.remove("bold");
+        boldTextContainer.classList.add("normal");
+        localStorage.setItem("bold", "normal");
+    }
+});
+
+normalTextInput.addEventListener("change", () => {
+    bold = false;
+});
+
+boldTextInput.addEventListener("change", () => {
+    bold = true;
 });
